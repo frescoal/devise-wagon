7cfbb137698b7c18f43ef3876d0840e3685dd8ad
Alain Fresco <alain.fresco@gmail.com>
2020-05-25 09:17:25 +0200

    Inital commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,20 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
   def home
+  end
+
+
+  def users_params
+  	params.require(:user).permit(:email,:password, :first_name, :last_name)
+  end
+
+  private
+  
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:color])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
